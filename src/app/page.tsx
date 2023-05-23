8fcea7b6c7ce165a770b340d5d428946df6b33e8
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-05-23 17:48:08 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ThemeToggle } from "@/components";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Next Tailwind Template</h1>
+      <ThemeToggle />
+    </div>
+  );
+}
