d87117df13beadcaae75ff41b7c2ba293e05a601
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-05-23 18:29:52 -0300

    First styles to navbar

--- src/components/ui/navbar/navbar.tsx
+++ src/components/ui/navbar/navbar.tsx
@@ -1,5 +1,15 @@
+"use client";
+
 import { FunctionComponent } from "react";
+import { Wrapper } from "@/components";
+import { cn } from "@/utils/cn";
 
 export const Navbar: FunctionComponent = () => {
-  return <header>Navbar</header>;
+  return (
+    <header
+      className={cn("sticky inset-x-0 top-0 h-20 bg-bg-secondary shadow", "flex items-center")}
+    >
+      <Wrapper>Navbar</Wrapper>
+    </header>
+  );
 };
