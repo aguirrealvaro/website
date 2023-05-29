d2f7052ed24f7b58760cc87da968bcb018267e8b
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-05-29 19:38:02 -0300

    mediaLinks constant

--- src/components/navbar/common/media-links.tsx
+++ src/components/navbar/common/media-links.tsx
@@ -2,25 +2,33 @@ import { FunctionComponent } from "react";
 import { Github, Linkedin } from "lucide-react";
 import { Icon, iconButtonVariants } from "@/components/ui";
 
+const mediaLinks = [
+  {
+    href: "https://github.com/aguirrealvaro",
+    icon: Github,
+  },
+  {
+    href: "https://www.linkedin.com/in/aguirrealvaro",
+    icon: Linkedin,
+  },
+];
+
 export const MediaLinks: FunctionComponent = () => {
   return (
     <>
-      <a
-        href="https://github.com/aguirrealvaro"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={iconButtonVariants()}
-      >
-        <Icon icon={Github} />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/aguirrealvaro/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={iconButtonVariants()}
-      >
-        <Icon icon={Linkedin} />
-      </a>
+      {mediaLinks.map(({ href, icon }, index) => {
+        return (
+          <a
+            key={index}
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={iconButtonVariants()}
+          >
+            <Icon icon={icon} />
+          </a>
+        );
+      })}
     </>
   );
 };
