b20b26b403f26ea932afe56e296739e2305479d2
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-05-29 19:23:08 -0300

    Init CVA variants on button

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,20 +1,36 @@
 import { ButtonHTMLAttributes, FunctionComponent, ReactNode } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/utils/cn";
+
+const buttonVariants = cva(
+  [
+    "flex items-center justify-center gap-2 bg-red-500",
+    "border border-transparent",
+    "disabled:cursor-not-allowed disabled:bg-disabled-primary disabled:text-disabled-secondary",
+  ],
+  {
+    variants: {
+      size: {
+        xs: "",
+        sm: "",
+        md: "",
+        lg: "",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+    },
+  }
+);
 
 type ButtonProps = {
   children: ReactNode;
-} & ButtonHTMLAttributes<HTMLButtonElement>;
+} & ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>;
 
-export const Button: FunctionComponent<ButtonProps> = ({ children, ...restProps }) => {
+export const Button: FunctionComponent<ButtonProps> = ({ children, size, ...restProps }) => {
   return (
-    <button
-      className={cn(
-        "flex items-center justify-center gap-2 bg-red-500",
-        "border border-transparent",
-        "disabled:cursor-not-allowed disabled:bg-disabled-primary disabled:text-disabled-secondary"
-      )}
-      {...restProps}
-    >
+    <button className={cn(buttonVariants({ size }))} {...restProps}>
       <span className="hidden disabled:text-disabled-secondary"></span>
       {children}
     </button>
