1084881fa078a7e591637e4c54ff04d51b29cdf4
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-05-29 19:55:01 -0300

    Add block!

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -7,9 +7,13 @@ const buttonVariants = cva(
     "flex items-center justify-center gap-2 bg-red-500",
     "border border-transparent",
     "disabled:cursor-not-allowed disabled:bg-disabled-primary disabled:text-disabled-secondary",
+    // TO DO: add transitions
   ],
   {
     variants: {
+      block: {
+        true: "w-full",
+      },
       size: {
         xs: "h-6 px-2 text-xs",
         sm: "h-8 px-3 text-sm",
@@ -35,9 +39,9 @@ type ButtonProps = {
   VariantProps<typeof buttonVariants>;
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, size, shape, ...restProps }, ref) => {
+  ({ children, block, size, shape, ...restProps }, ref) => {
     return (
-      <button ref={ref} className={cn(buttonVariants({ size, shape }))} {...restProps}>
+      <button ref={ref} className={cn(buttonVariants({ block, size, shape }))} {...restProps}>
         <span className="hidden text-sm"></span>
         {children}
       </button>
