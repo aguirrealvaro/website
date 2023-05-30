2a44952417774105193e1e80567404d7dde97809
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-05-29 22:07:57 -0300

    Remove unnecessary use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
-"use client";
-
 import { PageContainer, Wrapper } from "@/components";
-import { Button } from "@/components/ui";
 
 export default function Home() {
   return (
     <div>
       <Wrapper>
-        <PageContainer>
-          <Button variant="plain" colorScheme="yellow">
-            Boton
-          </Button>
-        </PageContainer>
+        <PageContainer>Content</PageContainer>
       </Wrapper>
     </div>
   );
