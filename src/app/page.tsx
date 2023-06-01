bffbfb3aa669427d4ee1e5f6903d9ed1f006cbac
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-01 15:23:35 -0300

    Introduction text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,30 @@ const Home = () => {
   return (
     <div>
       <Wrapper>
-        <PageContainer>Content</PageContainer>
+        <PageContainer>
+          <h1>Hello, there! Welcome to my personal website</h1>
+          <p>
+            I&apos;m Alvaro, a developer based in Buenos Aires, Argentina. With over 4 years
+            ofexperience in web development, I specialize in front-end development while also
+            having a solid background in back-end technologies.
+          </p>
+          <p>
+            I&apos;m passionate about modern web architectures, building user-friendly and
+            simple digital products. I believe in writing clean and efficient code that&apos;s
+            easy to maintain and scale.
+          </p>
+          <p>
+            I specialize in working with technologies such as React, TypeScript, Next.js, React
+            Query, Styled Components, Tailwind CSS, NodeJS, among others. I am always willing
+            to learn and stay up-to-date with the latest trends and technologies.
+          </p>
+          <p>
+            I&apos;ll be sharing some of my personal projects and publishing articles about
+            topics that interest me and things I&apos;ve learned throughout my career as a web
+            developer.
+          </p>
+          <p>I&apos; m currently looking for a new role as a developer. Hire me?</p>
+        </PageContainer>
       </Wrapper>
     </div>
   );
