2305c0c88c326aa2110a7a1b5def9376b1c98f66
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-12 18:19:33 -0300

    Create POST

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,16 +1,29 @@
 import { createHash } from "crypto";
-import type { NextApiRequest } from "next";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import prisma from "@/utils/prisma";
 
-export async function GET(request: NextApiRequest) {
-  const ipAddress = request.headers["x-forwarded-for"] || "0.0.0.0";
+export async function GET() {
+  const sessions = await prisma.session.findMany();
+
+  return NextResponse.json(sessions);
+}
+
+export async function POST() {
+  //const ipAddress = request.headers["x-forwarded-for"] || "0.0.0.0";
+
+  const headersList = headers();
+  const ipAddress = headersList.get("x-forwarded-for") || "0.0.0.0";
 
   const currentUserId = createHash("md5")
     .update(ipAddress + (process.env.IP_ADDRESS_SALT || ""), "utf8")
     .digest("hex");
 
-  const sessions = await prisma.session.findMany();
+  const newSession = await prisma.session.create({
+    data: {
+      id: currentUserId,
+    },
+  });
 
-  return NextResponse.json({ ipAddress, currentUserId, sessions });
+  return NextResponse.json(newSession);
 }
