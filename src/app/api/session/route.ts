b01df9a8b5c531c39f825780d5836af1e95ef79e
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-12 17:35:03 -0300

    Install and init prisma

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
+import prisma from "@/utils/prisma";
 
 export async function GET(request: NextApiRequest) {
   const ipAddress = request.headers["x-forwarded-for"] || "0.0.0.0";
 
-  return NextResponse.json({ ipAddress });
+  const sessions = await prisma.session.findMany();
+  return NextResponse.json({ ipAddress, sessions });
 }
