4a0917e816318c9b9c86c2ce8adb8152620f41c1
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-12 17:44:19 -0300

    Hash user ip

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,3 +1,4 @@
+import { createHash } from "crypto";
 import type { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 import prisma from "@/utils/prisma";
@@ -5,6 +6,11 @@ import prisma from "@/utils/prisma";
 export async function GET(request: NextApiRequest) {
   const ipAddress = request.headers["x-forwarded-for"] || "0.0.0.0";
 
+  const currentUserId = createHash("md5")
+    .update(ipAddress + (process.env.IP_ADDRESS_SALT || ""), "utf8")
+    .digest("hex");
+
   const sessions = await prisma.session.findMany();
-  return NextResponse.json({ ipAddress, sessions });
+
+  return NextResponse.json({ ipAddress, currentUserId, sessions });
 }
