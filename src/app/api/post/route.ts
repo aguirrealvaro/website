60a0f6b0d973fffd991eb69aa4e7c829c56bb0d2
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-13 04:50:11 -0300

    Get posts

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
-//import prisma from "@/utils/prisma";
+import prisma from "@/utils/prisma";
 
 export async function GET() {
-  //const posts = await prisma.post.findMany();
-  return NextResponse.json("posts");
+  const posts = await prisma.post.findMany();
+  return NextResponse.json(posts);
 }
