893fb9c32b1e2d2ef2a8bd7b0e98847d515c2433
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-13 04:56:57 -0300

    Include views & likes on posts

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 import prisma from "@/utils/prisma";
 
 export async function GET() {
-  const posts = await prisma.post.findMany();
+  const posts = await prisma.post.findMany({
+    include: { views: true, likes: true },
+  });
+
   return NextResponse.json(posts);
 }
