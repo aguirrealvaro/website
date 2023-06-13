f3df3e8dfaf6ad6b9e4679b2330f37a3c00824fa
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-12 23:30:50 -0300

    Include single

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 import prisma from "@/utils/prisma";
 
 export async function GET() {
-  const posts = await prisma.post.findMany({ include: { views: false, likes: false } });
+  const posts = await prisma.post.findMany({ include: { views: true } });
   return NextResponse.json(posts);
 }
