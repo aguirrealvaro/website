b39954a724d8f1f38098714dc2077b68addb821c
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-12 23:33:50 -0300

    Remove posts

--- src/app/api/post/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import prisma from "@/utils/prisma";
-
-export async function GET() {
-  const posts = await prisma.post.findMany();
-  return NextResponse.json(posts);
-}
