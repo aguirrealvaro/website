71918ddad7692842792cde93bbc47f4b64c3422c
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-19 19:30:32 -0300

    Create PostWrapper

---
+++ src/app/blog/[slug]/common/post-wrapper.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { FunctionComponent } from "react";
+import { PostHeader, PostContent } from ".";
+import { useSinglePost } from "@/hooks";
+import { allPosts } from "contentlayer/generated";
+
+type PostWrapperProps = {
+  slug: string;
+};
+
+const PostWrapper: FunctionComponent<PostWrapperProps> = ({ slug }) => {
+  const { post: relatedPost, isLoading } = useSinglePost(slug);
+
+  const pagePost = allPosts.find((post) => post.slug === slug);
+
+  if (!pagePost) return null;
+
+  const { title, publishedAt, body } = pagePost;
+
+  return (
+    <>
+      <PostHeader
+        title={title}
+        publishedAt={publishedAt}
+        views={relatedPost?.views}
+        likes={relatedPost?.likes.length}
+        isLoading={isLoading}
+      />
+      <PostContent content={body.code} />
+    </>
+  );
+};
+
+export { PostWrapper };
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import { FunctionComponent } from "react";
-import { PostContent, PostHeader } from "./common";
+import { PostWrapper } from "./common/post-wrapper";
 import { PageContainer, Wrapper } from "@/components";
-import { useSinglePost } from "@/hooks";
 import { allPosts } from "contentlayer/generated";
 
 type PostProps = {
@@ -13,25 +10,10 @@ type PostProps = {
 };
 
 const Post: FunctionComponent<PostProps> = ({ params }) => {
-  const { post: relatedPost, isLoading } = useSinglePost(params.slug);
-
-  const pagePost = allPosts.find((post) => post.slug === params.slug);
-
-  if (!pagePost) return null;
-
-  const { title, publishedAt, body } = pagePost;
-
   return (
     <PageContainer>
       <Wrapper>
-        <PostHeader
-          title={title}
-          publishedAt={publishedAt}
-          views={relatedPost?.views}
-          likes={relatedPost?.likes.length}
-          isLoading={isLoading}
-        />
-        <PostContent content={body.code} />
+        <PostWrapper slug={params.slug} />
       </Wrapper>
     </PageContainer>
   );
