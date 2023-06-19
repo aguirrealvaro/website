1314c8bf1829703d0a0d79f5e7b17115538c0312
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-19 20:45:42 -0300

    Init exit animations content

--- src/app/blog/[slug]/common/post-content.tsx
+++ src/app/blog/[slug]/common/post-content.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import { useMDXComponent } from "next-contentlayer/hooks";
+import { Typography } from "@/components/ui";
 
 type PostContentProps = {
   content: string;
@@ -8,7 +9,11 @@ type PostContentProps = {
 const PostContent: FunctionComponent<PostContentProps> = ({ content }) => {
   const MDXContent = useMDXComponent(content);
 
-  return <MDXContent />;
+  return <MDXContent components={components} />;
 };
 
 export { PostContent };
+
+const components = {
+  p: (props: any) => <Typography.Paragraph {...props} />,
+};
