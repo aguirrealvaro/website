cfd1e9d1b0aef3defa2ff88bd96b2f3e9846c8e7
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-23 15:28:05 -0300

    Get posts on posts list

--- src/components/posts-list/posts-list.tsx
+++ src/components/posts-list/posts-list.tsx
@@ -1,16 +1,21 @@
 import { FunctionComponent } from "react";
 import Link from "next/link";
 import { formatDate } from "@/utils/format-date";
+import prisma from "@/utils/prisma";
 import { allPosts } from "contentlayer/generated";
 
 type PostsListProps = {
   sliced?: boolean;
 };
 
-const PostsList: FunctionComponent<PostsListProps> = ({ sliced = false }) => {
+const PostsList: FunctionComponent<PostsListProps> = async ({ sliced = false }) => {
   const sortedPosts = allPosts.sort((a, b) => b.publishedAt.localeCompare(a.publishedAt));
 
   const displayPosts = sliced ? sortedPosts.slice(0, 3) : allPosts;
+
+  const posts = await prisma.post.findMany({
+    include: { likes: true },
+  });
 
   return (
     <ul>
@@ -18,13 +23,11 @@ const PostsList: FunctionComponent<PostsListProps> = ({ sliced = false }) => {
         const { title, slug, publishedAt, enabled } = post;
         const { dateString, formattedDate } = formatDate(publishedAt);
 
-        const renderMetric = (metric: number | undefined) => {
-          if (false) {
-            return "...";
-          } else {
-            return metric;
-          }
-        };
+        const relatedPost = posts?.find((post) => post.slug === slug);
+
+        if (!relatedPost) return null;
+
+        const { views, likes } = relatedPost;
 
         if (process.env.NODE_ENV === "production" && !enabled) return null;
 
@@ -33,8 +36,8 @@ const PostsList: FunctionComponent<PostsListProps> = ({ sliced = false }) => {
             <Link href={`/blog/${slug}`} className="flex flex-col py-4 transition">
               <h2 className="font-medium text-text-heading">{title}</h2>
               <span className="text-text-secondary">
-                <time dateTime={dateString}>{formattedDate}</time> · {renderMetric(5)} views ·{" "}
-                {renderMetric(5)} likes
+                <time dateTime={dateString}>{formattedDate}</time> · {views} views ·{" "}
+                {likes.length} likes
               </span>
             </Link>
           </li>
